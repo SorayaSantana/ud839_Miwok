2c0fb891ae7e300e8996b931d7fccc29ada9a7ed
Sory <sory.dakota@gmail.com>
2023-03-31 12:10:53 +0200

    Revert "Revert "Lesson one done.""
    This reverts commit 72e6c43227cbd1c0e64b5143696cd9dad1978043.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,7 +15,12 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 // import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,5 +32,46 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        TextView numbersView = (TextView) findViewById(R.id.numbers);
+        numbersView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this,NumbersActivity.class);
+                startActivity(numbersIntent);
+                Toast.makeText(view.getContext(), R.string.welcome_numbers, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView colorsView = (TextView) findViewById(R.id.colors);
+        colorsView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent colorsIntent = new Intent(MainActivity.this,ColorsActivity.class);
+                startActivity(colorsIntent);
+                Toast.makeText(view.getContext(), R.string.welcome_colors, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView familyView = (TextView) findViewById(R.id.family);
+        familyView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent familyIntent = new Intent(MainActivity.this,FamilyMembersActivity.class);
+                startActivity(familyIntent);
+                Toast.makeText(view.getContext(), R.string.welcome_family, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView phrasesView = (TextView) findViewById(R.id.phrases);
+        phrasesView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent phrasesIntent = new Intent(MainActivity.this,PhrasesActivity.class);
+                startActivity(phrasesIntent);
+                Toast.makeText(view.getContext(), R.string.welcome_phrases, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
