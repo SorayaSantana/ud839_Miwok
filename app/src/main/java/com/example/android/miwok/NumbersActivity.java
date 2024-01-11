dbc5903896d0f2bec07970c66c309dd10d2d9309
Sory <sory.dakota@gmail.com>
2024-01-11 15:05:02 +0100

    Lesson one done. - Create new activities and intents to navigate to those views. - show some toast info when entering. - Create listeners to navigate

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,21 +1,53 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
+
+
+    private static MediaPlayer mMediaPlayer;
+
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
+    }
+
+    private MediaPlayer.OnCompletionListener mCompletitionListener = new MediaPlayer.OnCompletionListener(){
+        @Override
+        public void onCompletion(MediaPlayer mp){
+            releaseMediaPlayer();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("one","lutti"));
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one","lutti", R.raw.number_one));
         words.add(new Word("two", "ottiko"));
         words.add(new Word("three", "tolookosu"));
         words.add(new Word("four", "oyyisa"));
@@ -29,6 +61,22 @@ public class NumbersActivity extends AppCompatActivity {
         WordAdapter itemsAdapter = new WordAdapter(this, words);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("","index = " + i);
+                releaseMediaPlayer();
+
+                if(words.get(i).hasSound()){
+
+                    Log.d("", "calling media player word with sound = "+ words.get(i).hasSound());
+                    mMediaPlayer = MediaPlayer.create(NumbersActivity.this, words.get(i).getSoundId());
+                    mMediaPlayer.start();
+                    mMediaPlayer.setOnCompletionListener(mCompletitionListener);
+                }
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,12 +1,30 @@
 package com.example.android.miwok;
 
+import android.util.Log;
+
 public class Word {
     private String mMiWokWord;
     private String mDefaultLangWord;
 
+    private Integer mSoundId;
+
     public Word(String defLangWord, String miWok){
         mMiWokWord = miWok;
         mDefaultLangWord = defLangWord;
+    }
+    public Word(String defLangWord, String miWok, Integer soundId){
+        mMiWokWord = miWok;
+        mDefaultLangWord = defLangWord;
+        mSoundId = soundId;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mMiWokWord='" + mMiWokWord + '\'' +
+                ", mDefaultLangWord='" + mDefaultLangWord + '\'' +
+                ", mSoundId=" + mSoundId +
+                '}';
     }
 
     public String getMiWokWord(){
@@ -16,4 +34,8 @@ public class Word {
     public String getDefaultLangWord(){
         return mDefaultLangWord;
     }
+    public Integer getSoundId(){
+        return mSoundId;
+    }
+    public boolean hasSound() { return mSoundId!=null;}
 }
